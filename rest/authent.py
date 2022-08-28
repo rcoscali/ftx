eb52a0591f1fdf2b6c88470ee3e22c0ba8496be2
Rémi COHEN SCALI <remi@cohenscali.net>
2022-08-28 11:48:46 +0200

    Handle URL Encoded subaccount name
    use format for correct display of some string

--- rest/authent.py
+++ rest/authent.py
@@ -1,16 +1,32 @@
 import time
 import hmac
 import json
+from urllib.parse import urlencode, quote
 from requests import Request
 from client import FtxClient
 
 ts = int(time.time() * 1000)
 
-# Read authent data from 'authen.data' file
+# Read authent data from 'authent.data' file
+# This file is a json for the FtxClient dict
+# provided as params at client instanciation
+# It has the following format:
+# {
+#     "api_key" : "<YourAPIKey>",
+#     "api_secret" : "<YourAPIKeySecret>",
+#     "subaccount_name" : "<YourURIEncodedSubAccountName>"
+# }
+
 with open('authent.data') as f:
     ftxclient_params_str = f.read()
 
+# Handle URLEncoded subaccount name
 ftxclient_params = json.loads(ftxclient_params_str)
+ftxclient_subaccount_str = ftxclient_params['subaccount_name']
+ftxclient_subaccount = urlencode({'subaccount_name':ftxclient_subaccount_str}, quote_via=quote)
+ftxclient_params_eqix = ftxclient_subaccount.find("=") + 1
+ftxclient_params['subaccount_name'] = ftxclient_subaccount[ftxclient_params_eqix:]
+print('Sub account name: "{}"'.format(ftxclient_params['subaccount_name']))
 
 # Instanciating client
 print('Instanciating FTX REST client ...')
@@ -44,7 +60,7 @@ print('	   Got ', len(ftxpositions), ' positions')
 #print(ftxpositions)
 
 for position in ftxpositions:
-    print('    Position "', position['future'], '"')
+    print('    Position "{}"'.format(position['future']))
     print('	   Side: ', position['side'])
     print('	   Size: ', position['size'])
     print('	   Cost: ', position['cost'])
